a0a0e2234573cf81f6d5d37f957ff6c2866201b4
muchrizfanani <muchrizfanani@gmail.com>
2025-03-13 00:23:18 +0700

    codelab 2 modul 2 fixed

--- CODELAB/MODUL_2/codelab2/src/Main.java
+++ CODELAB/MODUL_2/codelab2/src/Main.java
@@ -2,6 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        RekeningBank rek1 = new RekeningBank();
+        RekeningBank rek2 = new RekeningBank();
+
+        rek1.nomorR = "202410370110503";
+        rek1.namaP = "zhulfikar";
+        rek1.saldo = 2000000;
+
+        rek2.nomorR = "202410370110509";
+        rek2.namaP = "wahyu";
+        rek2.saldo = 500000;
+
+        rek1.tampilkanInfo();
+        rek2.tampilkanInfo();
+
+        rek1.setorUang(200000);
+        rek2.setorUang(100000);
+        System.out.println();
+
+        rek1.tarikTunai(100000);
+        rek2.tarikTunai(1000000);
+
         }
     }
-}
--- CODELAB/MODUL_2/codelab2/src/RekeningBank.java
+++ CODELAB/MODUL_2/codelab2/src/RekeningBank.java
@@ -1,3 +1,32 @@
 public class RekeningBank {
+ String nomorR,namaP;
+ double saldo;
+
+ void tampilkanInfo(){
+     System.out.println("===============================");
+     System.out.println("nomor rekening : " + nomorR);
+     System.out.println("nama pemilik : " + namaP);
+     System.out.println("saldo : " +"Rp."+saldo);
+     System.out.println("===============================\n");
+ }
+
+ void setorUang(double jumlah){
+     if (jumlah >0 ){
+         saldo += jumlah;
+         System.out.println(namaP + " " + "menyetor Rp." + jumlah + " " + "saldo sekarang Rp." + saldo);
+     }
+ }
+
+ void tarikTunai(double jumlah){
+     if(saldo > jumlah || saldo == jumlah){
+         saldo -= jumlah;
+         System.out.println(namaP + " " + "menarik Rp." + jumlah + " " + "saldo saat ini Rp." + saldo );
+     }else{
+         System.out.println(namaP + " " + "menarik Rp." + jumlah + " " + "(gagal ! saldo anda tidak cukup)" + " " + "saldo saat ini Rp." + saldo );
+
+     }
+ }
+
+
 
 }
