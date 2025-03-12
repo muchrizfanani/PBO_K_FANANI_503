1202992adf17a983e262b0f01037ec63046a50ab
muchrizfanani <muchrizfanani@gmail.com>
2025-03-12 19:59:08 +0700

    codelab 1 completed

--- CODELAB/MODUL_2/codelab1/src/Hewan.java
+++ CODELAB/MODUL_2/codelab1/src/Hewan.java
@@ -8,14 +8,14 @@ public class Hewan {
     String suara;
     Scanner ip = new Scanner(System.in); //menggunakan scanner sebagai atribut di class hewan
 
-    //method untuk meminta user untuk menginputkan data menggunakan scanner
-    void inputDataHewan(){
+    //method untuk meminta user untuk menginputkan data hewan menggunakan scanner
+    void inputDataHewan(Scanner ip){
         System.out.print("Masukkan Nama : ");
         this.nama = ip.nextLine();
         System.out.print("Masukkan Jenis : ");
-        this.nama = ip.nextLine();
+        this.jenis = ip.nextLine();
         System.out.print("Masukkan Suara : ");
-        this.nama = ip.nextLine();
+        this.suara = ip.nextLine();
     }
     void tampilkanInfo(){
         System.out.println("Nama = " + nama );
--- CODELAB/MODUL_2/codelab1/src/Main.java
+++ CODELAB/MODUL_2/codelab1/src/Main.java
@@ -6,14 +6,13 @@ public class Main {
         Hewan hewan1 = new Hewan();
         Hewan hewan2 = new Hewan();
 
-        hewan1.inputDataHewan(); //memanggil method di class Hewan untuk meminta input dari user dari objek hewan1
+        hewan1.inputDataHewan(ip); //memanggil method di class Hewan untuk meminta input dari user dari objek hewan1
         System.out.println();
-        hewan2.inputDataHewan(); //memanggil method di class Hewan untuk meminta input dari user dari objek hewan2
+        hewan2.inputDataHewan(ip); //memanggil method di class Hewan untuk meminta input dari user dari objek hewan2
 
-        System.out.println("======== Informasi Hewan =======");
+        System.out.println("\n======== Informasi Hewan =======");
         hewan1.tampilkanInfo(); // memanggil method di class hewan untuk menampilkan data dari hewan 1
         hewan2.tampilkanInfo();// memanggil method di class hewan untuk menampilkan data dari hewan 2
-
         ip.close();
 
         }
