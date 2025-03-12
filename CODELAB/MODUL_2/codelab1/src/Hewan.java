80dbcf2974b988a4f12158d68ee40f728e6b4128
muchrizfanani <muchrizfanani@gmail.com>
2025-03-12 14:18:35 +0700

    modul 2 fix

--- CODELAB/MODUL_2/codelab1/src/Hewan.java
+++ CODELAB/MODUL_2/codelab1/src/Hewan.java
@@ -1,18 +1,25 @@
-public class hewan {
+public class Hewan {
+
+    //atribut (data)
     String nama;
     String jenis;
     String suara;
+    int jumlah ;
 
-    public hewan (String nama,String jenis, String suara ){
+    //constructor
+    public Hewan(String nama, String jenis, String suara, int jumlah){
         this.nama = nama;
         this.jenis = jenis;
         this.suara = suara;
+        this.jumlah = jumlah;
     }
 
+    //method
     public void tampilkanInfo(){
         System.out.println("Nama = " + nama );
         System.out.println("Jenis = " + jenis );
         System.out.println("Suara = " + suara );
+        System.out.println("jumlah = " + jumlah );
         System.out.println();
 
     }
--- CODELAB/MODUL_2/codelab1/src/Main.java
+++ CODELAB/MODUL_2/codelab1/src/Main.java
@@ -2,8 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        hewan hewan1 = new hewan ("Kucing","mamalia", "miauw");
-        hewan hewan2 = new hewan ("Rifky", "amfibi", "ubekkk");
+        Hewan hewan1 = new Hewan("Kucing","mamalia", "miauw", 4);
+        Hewan hewan2 = new Hewan("Rifky", "amfibi", "ubekkk" ,10);
 
         hewan1.tampilkanInfo();
         hewan2.tampilkanInfo();
